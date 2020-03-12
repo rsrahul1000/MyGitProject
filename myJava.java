c14089bc25d7ab9231510fdc8e3d8d879357b889
Rahul S Sharma <rsrahul1000@gmail.com>
2020-03-12 21:03:48 +0530

    Update myJava.java

--- myJava.java
+++ myJava.java
@@ -1,6 +1,6 @@
 class myJava{
 	public static void main(String []args){
-		System.out.print("Yo git.... Welcome to my world.");
+		System.out.print("Yo git.... Welcome to my world");
 	}
 	void myMethod(){
 		sysout("MyMethod");
